acd8fa0ed1050436d6fcb178b5e497df5fa6dd35
JIAWEI68 <83011056+JIAWEI68@users.noreply.github.com>
2021-12-30 23:17:03 +0800

    DBAV Assignment V2

--- models/RestaurantDB.js
+++ models/RestaurantDB.js
@@ -7,43 +7,43 @@ class RestaurantDB{
     db.query(sql,callback);
     }
     getAllRestaurantCuisine(callback){
-    var sql = "SELECT cuisine from reviews.restaurant"
+    var sql = "SELECT * cuisine from reviews.restaurant"
     db.query(sql,callback);
     }
     getJapRestaurant(callback){
-    var sql = "SELECT * FROM reviews.restaurant WHERE cuisine = 'Japenese'"
+    var sql = "SELECT rest.id AS id, rest.name AS name, rest.location AS location, rest.map AS map, rest.telephone_number AS telephone_number, rest.opening_hours AS opening_hours, rest.description AS description, rest.rating AS rating, rest.cuisine AS cuisine, rest.compass_location AS compass_location, rest.title_picture AS title_picture, rest.description_picture AS description_picture FROM reviews.restaurant rest WHERE cuisine = 'Japenese'"
     db.query(sql,callback)
     }
     getChineseRestaurant(callback){
-    var sql = "SELECT * FROM reviews.restaurant WHERE cuisine = 'Chinese'"
+    var sql = "SELECT rest.id AS id, rest.name AS name, rest.location AS location, rest.map AS map, rest.telephone_number AS telephone_number, rest.opening_hours AS opening_hours, rest.description AS description, rest.rating AS rating, rest.cuisine AS cuisine, rest.compass_location AS compass_location, rest.title_picture AS title_picture, rest.description_picture AS description_picture FROM reviews.restaurant rest WHERE cuisine = 'Chinese'"
     db.query(sql,callback)
     }
     getWesternRestaurant(callback){
-    var sql = "SELECT * FROM reviews.restaurant WHERE cuisine = 'Western'"
+    var sql = "SELECT a.id, a.name, a.location, a.map, a.telephone_number, a.opening_hours, a.description, a.rating, a.cuisine, a.compass_location, a.title_picture, a.description_picture FROM reviews.restaurant a WHERE a.cuisine IN ('Western')"
     db.query(sql,callback)
     }
     getIndianRestaurant(callback){
-    var sql = "SELECT * FROM reviews.restaurant WHERE cuisine = 'Indian'"
+    var sql = "SELECT rest.id AS id, rest.name AS name, rest.location AS location, rest.map AS map, rest.telephone_number AS telephone_number, rest.opening_hours AS opening_hours, rest.description AS description, rest.rating AS rating, rest.cuisine AS cuisine, rest.compass_location AS compass_location, rest.title_picture AS title_picture, rest.description_picture AS description_picture FROM reviews.restaurant rest WHERE cuisine = 'Indian'"
     db.query(sql,callback)
     }
     getMalayRestaurant(callback){
-    var sql = "SELECT * FROM reviews.restaurant WHERE cuisine = 'Malay'"
+    var sql = "SELECT b.id,b.name,b.location,b.map,b.telephone_number,b.opening_hours,b.description,b.rating,b.cuisine,b.compass_location,b.title_picture,b.description_picture FROM reviews.restaurant  WHERE cuisine = 'Malay'"
     db.query(sql,callback)
     }
     getNorthRestaurant(callback){
-    var sql = "SELECT * FROM reviews.restaurant WHERE compass_location = 'North'"
+    var sql = "SELECT c.id,c.name,c.location,c.map,c.telephone_number,c.opening_hours,c.description,c.rating,c.cuisine,c.compass_location,c.title_picture,c.description_picture FROM reviews.restaurant WHERE compass_location = 'North'"
     db.query(sql,callback);
     }
     getSouthRestaurant(callback){
-    var sql = "SELECT * FROM reviews.restaurant WHERE compass_location = 'South'"
+    var sql = "SELECT rest.id AS id, rest.name AS name, rest.location AS location, rest.map AS map, rest.telephone_number AS telephone_number, rest.opening_hours AS opening_hours, rest.description AS description, rest.rating AS rating, rest.cuisine AS cuisine, rest.compass_location AS compass_location, rest.title_picture AS title_picture, rest.description_picture AS description_picture FROM reviews.restaurant rest WHERE compass_location = 'South'"
     db.query(sql,callback);
     }
     getEastRestaurant(callback){
-    var sql = "SELECT * FROM reviews.restaurant WHERE compass_location = 'East'"
+    var sql = "SELECT rest.id AS id, rest.name AS name, rest.location AS location, rest.map AS map, rest.telephone_number AS telephone_number, rest.opening_hours AS opening_hours, rest.description AS description, rest.rating AS rating, rest.cuisine AS cuisine, rest.compass_location AS compass_location, rest.title_picture AS title_picture, rest.description_picture AS description_picture FROM reviews.restaurant rest WHERE compass_location = 'East'"
     db.query(sql,callback);
     }
     getWestRestaurant(callback){
-    var sql = "SELECT * FROM reviews.restaurant WHERE compass_location = 'West'"
+    var sql = "SELECT rest.id AS id, rest.name AS name, rest.location AS location, rest.map AS map, rest.telephone_number AS telephone_number, rest.opening_hours AS opening_hours, rest.description AS description, rest.rating AS rating, rest.cuisine AS cuisine, rest.compass_location AS compass_location, rest.title_picture AS title_picture, rest.description_picture AS description_picture FROM reviews.restaurant rest WHERE compass_location = 'West'"
     db.query(sql,callback)
     }
     
