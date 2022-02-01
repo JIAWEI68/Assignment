29e238b79baa61f2279a69d8dbfcc137a42c7bc1
JIAWEI68 <ang.jiawei56@gmail.com>
2022-02-01 19:52:44 +0800

    search can get array

--- public/js/search.js
+++ public/js/search.js
@@ -1,9 +1,10 @@
 function getSearchResult(){
     var search = new XMLHttpRequest();
-    search.open("POST", "127.0.0.1:3030/restaurant/search",true);
+    search.open("POST", "http://127.0.0.1:3030/restaurant/search",true);
     search.setRequestHeader("Content-Type", "application/json");
     search.onload = function() {
         search_result_array = JSON.parse(search.responseText);
+        console.log(search_result_array);
     }
     var name = document.getElementById("name").value;
     var payload = {
